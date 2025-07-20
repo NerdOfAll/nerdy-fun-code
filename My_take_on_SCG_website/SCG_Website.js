d304ab69ab1ce16ceff767126078441bb08c0c3f
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-07-20 10:37:49 -0600

    Nearly there!

--- My_take_on_SCG_website/SCG_Website.js
+++ My_take_on_SCG_website/SCG_Website.js
@@ -1,3 +1,4 @@
+/* This following stuff is for getting the little "this goes to a differnet webpage" image to show up to links */
 const extSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
   <path fill-rule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z" clip-rule="evenodd" />
 </svg>
@@ -36,3 +37,17 @@ function setup() {
     linkify(aTags[i]);
   }
 }
+
+/* This will be for the rotating images of SCG merch */
+let goHereImages = ["https://bi.fbcd.co/posts/add-texture-to-your-hoodie-drawing-exercise-63dc28e5827f6916144278.jpg", "https://i.etsystatic.com/9172517/r/il/67c1d3/1077168561/il_340x270.1077168561_iyjk.jpg", "https://i.pinimg.com/736x/70/e1/e2/70e1e299af15a6d82bdd55eb2e68620d.jpg"]
+let x = 0
+function change() {
+    if (x < goHereImages.length-1) {
+        x += 1
+    } else {
+        x = 0
+    }
+    let imageThing=document.getElementById("merch_img")
+    imageThing.src=goHereImages[x]
+}
+setInterval(change, 5000)
